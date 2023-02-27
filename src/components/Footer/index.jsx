24b30ea7c303285e6af55fd71816119404ef6579
Guilherme Henrique <guilhermemalka6@gmail.com>
2023-02-27 09:07:15 -0300

    edit footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
 import * as styles from './styles.module.css'
-import bohrPowered from '../../assets/images/logo-a4t.png'
+import bohrPowered from '../../assets/svg/bohr_powered.svg'
 
 const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
       <div>
-        <img src={bohrPowered} alt="powered by A4T" />
+        <img src={bohrPowered} alt="powered by bohr.io logo" />
       </div>
     </footer>  
   )
